053cecf47980149cbcf98a93d9fa6eb4841ccbfc
krisbobo <kiyongbohbo@gmail.com>
2022-10-24 15:53:36 +0100

    Add functions to save and update book info

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Create array of books
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const addBook = document.querySelector('#book-collection');
+const btn = document.querySelector('#add-button');
+
+if (!localStorage.getItem('bookInfo')) {
+    localStorage.setItem('bookInfo', JSON.stringify([]));
+}
+
+let books;
+
+function userInterface() {
+    addBook.innerHTML = '';
+    books.forEach((data, index) => {
+        const newBook =document.createElement('div');
+        newBook.classList.add('new-book');
+        const text =document.createElement('p');
+        text.textContent = `${data.title} By ${data.author}`;
+        const removebtn = document.createElement('button');
+        removebtn.textContent = 'Remove';
+        removebtn.addEventListener('click', removebtn.bind(index));
+        newBook.appendChild(text);
+        newBook.appendChild(removebtn);
+        addBook.appendChild(newBook);
+    });
+}
+
+function displayBook() {
+    books = JSON.parse(localStorage.getItem('bookInfo'));
+    userInterface ();
+}
+displayBook ();
+
+function saveBooks(book) {
+    localStorage.setItem('bookInfo', JSON.stringify(book));
+}
