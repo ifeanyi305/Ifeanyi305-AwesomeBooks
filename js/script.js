664f6fe9013009b4b1f55349f44c201b5c78ace0
krisbobo <kiyongbohbo@gmail.com>
2022-10-24 19:05:43 +0100

    Fix file structure and linter errors

--- js/script.js
+++ js/script.js
@@ -5,57 +5,58 @@ const addBook = document.querySelector('#book-collection');
 const btn = document.querySelector('#add-button');
 
 if (!localStorage.getItem('bookInfo')) {
-    localStorage.setItem('bookInfo', JSON.stringify([]));
+  localStorage.setItem('bookInfo', JSON.stringify([]));
 }
 
 let books;
 
-function userInterface() {
-    addBook.innerHTML = '';
-    books.forEach((data, index) => {
-        const newBook = document.createElement('div');
-        newBook.classList.add('new-book');
-        const text = document.createElement('p');
-        text.textContent = `${data.title}`;
-        const text2 = document.createElement('p');
-        text2.textContent = ` ${data.author}`;
-        const removebtn = document.createElement('button');
-        removebtn.textContent = 'Remove';
-        removebtn.addEventListener('click', removeBook.bind(index));
-        newBook.appendChild(text2);
-        newBook.appendChild(text);
-        newBook.appendChild(removebtn);
-        addBook.appendChild(newBook);
-    });
-}
-
-function removeBook() {
-    books.splice(this, 1);
-    saveBooks(books);
-    displayBook();
+function saveBooks(book) {
+  localStorage.setItem('bookInfo', JSON.stringify(book));
 }
 
 function displayBook() {
-    books = JSON.parse(localStorage.getItem('bookInfo'));
-    userInterface ();
+  books = JSON.parse(localStorage.getItem('bookInfo'));
+  // eslint-disable-next-line no-use-before-define
+  userInterface();
 }
-displayBook ();
+displayBook();
 
-function saveBooks(book) {
-    localStorage.setItem('bookInfo', JSON.stringify(book));
+function removeBook() {
+  books.splice(this, 1);
+  saveBooks(books);
+  displayBook();
+}
+
+function userInterface() {
+  addBook.innerHTML = '';
+  books.forEach((data, index) => {
+    const newBook = document.createElement('div');
+    newBook.classList.add('new-book');
+    const text = document.createElement('p');
+    text.textContent = `${data.title}`;
+    const text2 = document.createElement('p');
+    text2.textContent = ` ${data.author}`;
+    const removebtn = document.createElement('button');
+    removebtn.textContent = 'Remove';
+    removebtn.addEventListener('click', removeBook.bind(index));
+    newBook.appendChild(text2);
+    newBook.appendChild(text);
+    newBook.appendChild(removebtn);
+    addBook.appendChild(newBook);
+  });
 }
 
 btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (title.value && author.value) {
-        const bookObject = {
-            title: title.value,
-            author: author.value,
-        }
-        books.push(bookObject);
-        saveBooks(books);
-        displayBook();
-        title.value = '';
-        author.value = '';
-    } 
+  e.preventDefault();
+  if (title.value && author.value) {
+    const bookObject = {
+      title: title.value,
+      author: author.value,
+    };
+    books.push(bookObject);
+    saveBooks(books);
+    displayBook();
+    title.value = '';
+    author.value = '';
+  }
 });
